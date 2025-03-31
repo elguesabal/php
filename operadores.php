dfdcb55c3fbc17eaf8b6ce76cfced82e087ceb97
elguesabal <joseelguesabal@gmail.com>
2025-03-31 15:52:14 -0300

    adicionando estudos de variaveis e operadores

---
+++ operadores.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>operadores</title>
+</head>
+<body>
+    <?php
+        $n1 = 10;
+        $n2 = 20;
+        $n3 = $n1 + $n2;
+        echo $n3, "<br/>";
+        echo "vampeta: ", ($n1 + $n2);
+    ?>
+    <br/><br/>
+    <?php
+        $n1 = $_GET["a"];
+        $n2 = $_GET["b"];
+        $n3 = $n1 + $n2;
+        echo $n3, "<br/>";
+        echo "vampeta: ", ($n1 + $n2);
+    ?>
+    <br/><br/>
+    <?php
+        $n1 = $_GET["a"];
+        $n2 = $_GET["b"];
+        echo abs($n1);
+        echo "<br/>";
+        echo abs($n2);
+    ?>
+    <br/><br/>
+    <?php
+        $n1 = $_GET["a"];
+        $n2 = $_GET["b"];
+        echo "ue: ", (4 ^ 2);
+    ?>
+</body>
+</html>
---
+++ variaveis.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variaveis</title>
+</head>
+<body>
+   <?php
+        $s = "vampeta: ";
+        $n = 42;
+        echo $s, $n, "<br/>";
+        $string = $s. $n;
+        echo "$string<br/>";
+        echo '$string<br/>';
+   ?> 
+</body>
+</html>
