cc0d17374de25f29bd69f5818a75454970f8a3d9
elguesabal <joseelguesabal@gmail.com>
2025-04-07 17:50:34 -0300

    adicionando estudos de switch case

---
+++ switch.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        switch (2) {
+            case 1:
+                echo "1";
+                break;
+            case 2:
+                echo "2";
+                break;
+            case 3:
+                echo "3";
+                break;
+            default:
+                echo "?";
+        }
+    ?>
+</body>
+</html>
