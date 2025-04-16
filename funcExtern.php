52451ca30add2b977f4114ab15a11f9cdca7bd9f
elguesabal <joseelguesabal@gmail.com>
2025-04-16 18:53:33 -0300

    estudando funcoes e includes

---
+++ funcExtern.php
@@ -0,0 +1,5 @@
+<?php
+    function vampeta() {
+        echo "vampeta";
+    }
+?>
---
+++ function.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // function soma($a, $b) {
+        //     return $a + $b;
+        // }
+        // echo soma(5, 5);
+
+        // function soma() {
+        //     $p = func_get_args();
+        //     $t = func_num_args();
+        //     $s = 0;
+
+        //     for ($i = 0; $i < $t; $i++) {
+        //         $s += $p[$i];
+        //     }
+        //     return $s;
+        // }
+        // echo soma(1, 2, 3, 4, 5);
+
+        // function teste(&$x) {
+        //     $x += 2;
+        //     echo $x;
+        // }
+        // $a = 3;
+        // teste($a);
+        // echo " ", $a;
+
+        // include "funcExtern.php";
+        // require "funcExtern.php";
+        // include_once "funcExtern.php";
+        require_once "funcExtern.php";
+        vampeta();
+    ?>
+</body>
+</html>
